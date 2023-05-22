408a1e66e0104781513b190b9cc27be78d604071
shwetanshu-cldcvr <shwetanshu@cldcvr.com>
2023-05-23 03:42:19 +0530

    Updated templates and secret manager script

--- assets/scripts/aws-secret-manager.sh
+++ assets/scripts/aws-secret-manager.sh
@@ -5,7 +5,7 @@ set -e
 function load_env_variables() {
   file_path="$1"
   env_vars=$(<"$file_path")
-  echo "$env_vars"
+  echo $env_vars
 }
 
 # Check if the secret already exists in AWS Secrets Manager
@@ -17,8 +17,8 @@ function check_secret_exists() {
 
 # Create or update the secret in AWS Secrets Manager
 function create_or_update_secret() {
-  secret_name="$1"
-  env_vars="$2"
+  secret_name=$1
+  env_vars=$2
 
   secret_string="{"
   while IFS="=" read -r key value; do
@@ -28,25 +28,25 @@ function create_or_update_secret() {
 
   if check_secret_exists "$secret_name"; then
     # Clear the existing secret content
-    aws secretsmanager update-secret --secret-id "$secret_name" --secret-string "{}"
+    aws secretsmanager update-secret --secret-id $secret_name --secret-string "{}"
 
     # Update the secret with the new content
-    aws secretsmanager update-secret --secret-id "$secret_name" --secret-string "$secret_string"
+    aws secretsmanager update-secret --secret-id $secret_name --secret-string $secret_string
   else
-    aws secretsmanager create-secret --name "$secret_name" --secret-string "$secret_string"
+    aws secretsmanager create-secret --name $secret_name --secret-string $secret_string
   fi
 }
 
 
 # Get the ARN of the secret
 function get_secret_arn() {
-  secret_name="$1"
-  aws secretsmanager describe-secret --secret-id "$secret_name" | jq -r '.ARN'
+  secret_name=$1
+  aws secretsmanager describe-secret --secret-id $secret_name | jq -r '.ARN'
 }
 
 # Get the secret content
 function get_secret_content() {
-  secret_name="$1"
-  aws secretsmanager get-secret-value --secret-id "$secret_name" | jq -r '.SecretString'
+  secret_name=$1
+  aws secretsmanager get-secret-value --secret-id $secret_name | jq -r '.SecretString'
 }
 
